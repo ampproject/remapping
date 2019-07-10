9276f939ad08aeb3ca3268e79bfae6e1e58f8a9b
Justin Ridgewell <jridgewell@google.com>
2019-07-10 15:24:19 -0400

    Move jest config out of package.json

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  transform: {
+    '.ts': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  testRegex: 'test\\.ts$',
+  moduleFileExtensions: ['ts', 'js'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/test/'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
+  collectCoverageFrom: ['src/*.{js,ts}'],
+};
