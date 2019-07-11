c4fde9a8c173871f82e2c814dd817efffc5c788e
Justin Ridgewell <jridgewell@google.com>
2019-07-10 22:45:25 -0400

    Add concatenation test case

---
+++ test/samples/transpile-concatenate/files/bundle.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var a = (function () {
+  return 'a';
+});
+
+var b = (function () {
+  return 'b';
+});
+
+exports.a = a;
+exports.b = b;
+//# sourceMappingURL=bundle.js.map
---
+++ test/samples/transpile-concatenate/test.ts
@@ -0,0 +1,52 @@
+import { readFileSync, writeFileSync } from 'fs';
+import { RawSourceMap, SourceMapConsumer } from 'source-map';
+import resorcery from '../../../src/resorcery';
+
+function read(filename: string): string {
+  return readFileSync(`${__dirname}/files/${filename}`, 'utf8');
+}
+
+describe('transpile then concatenate', () => {
+  test('concated sections point to source files', () => {
+    const map = read('bundle.js.map');
+    const remapped = resorcery(map, {
+      'a.js': read('a.js.map'),
+      'b.js': read('b.js.map'),
+    });
+
+    const consumer = new SourceMapConsumer((remapped as unknown) as RawSourceMap);
+
+    const a = consumer.originalPositionFor({
+      column: 11,
+      line: 6,
+    });
+    expect(a).toMatchObject({
+      column: 21,
+      line: 1,
+      source: 'a.mjs',
+    });
+
+    const b = consumer.originalPositionFor({
+      column: 11,
+      line: 10,
+    });
+    expect(b).toMatchObject({
+      column: 21,
+      line: 1,
+      source: 'b.mjs',
+    });
+  });
+
+  test('inherits sourcesContent of original source', () => {
+    const map = read('bundle.js.map');
+    const remapped = resorcery(map, {
+      'a.js': read('a.js.map'),
+      'b.js': read('b.js.map'),
+    });
+
+    expect(remapped.sourcesContent).toEqual([
+      read('a.mjs'),
+      read('b.mjs'),
+    ]);
+  });
+});
