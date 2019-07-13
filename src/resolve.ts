716895eda9eb5f964262f0c6f786079e8172a4d3
Justin Ridgewell <jridgewell@google.com>
2019-07-13 04:29:34 -0400

    Use URL to resolve all URLs

--- src/resolve.ts
+++ src/resolve.ts
@@ -1,5 +1,3 @@
-import { relative as pathRelative, resolve as pathResolve } from 'path';
-
 type WhatWgUrl = import('url').URL;
 interface Url extends WhatWgUrl {
   new (input: string, base?: string): WhatWgUrl;
@@ -9,17 +7,16 @@ declare var URL: unknown;
 /* istanbul ignore next */
 const Url = (typeof URL !== 'undefined' ? URL : require('url').URL) as Url;
 
+// Matches "..", which must be preceeded by "/" or the start of the string, and
+// must be followed by a "/".
+const parentRegex = /(^|\/)\.\.(?:\/)/g;
+
 function isAbsoluteUrl(url: string): boolean {
   try {
     return !!new Url(url);
   } catch (e) {
     return false;
   }
-}
-
-function withTrailingSlash(str: string): string {
-  if (!str.endsWith('/')) str += '/';
-  return str;
 }
 
 function isProtocolRelativeUrl(url: string): boolean {
@@ -30,9 +27,60 @@ function isPathAbsolute(path: string): boolean {
   return path.startsWith('/');
 }
 
-function resolveProtocolRelative(input: string, base: string): string {
-  const { href, protocol } = new Url(input, base);
+/**
+ * Creates a directory name that is guaranteed to not be in `str`.
+ */
+function uniqInStr(str: string): string {
+  let uniq = String(Math.random()).slice(2);
+  while (str.indexOf(uniq) > -1) {
+    /* istanbul ignore next */
+    uniq += uniq;
+  }
+  return `z${uniq}/`;
+}
+
+/**
+ * Resolves a protocol relative URL, but keeps it protocol relative.
+ * Essentially just for the normalization.
+ */
+function resolveProtocolRelative(input: string, absoluteBase: string): string {
+  const { href, protocol } = new Url(input, absoluteBase);
   return href.slice(protocol.length);
+}
+
+/**
+ * Resolves a relative URL, while keeping it relative.
+ * Essentially just for the normalization.
+ */
+function resolveToAfterOrigin(input: string): string {
+  const { href } = new Url(input, 'https://foo.com/');
+  return href.slice('https://foo.com/'.length);
+}
+
+function resolvePath(input: string, base: string): string {
+  const joined = isPathAbsolute(input) ? input : base + input;
+  if (!parentRegex.test(joined)) {
+    return resolveToAfterOrigin(joined);
+  }
+
+  const uniq = uniqInStr(joined);
+  let prefix = '';
+  let total = 0;
+  do {
+    prefix += uniq;
+    total++;
+  } while (parentRegex.test(joined));
+
+  if (joined.startsWith('/')) prefix = prefix.slice(0, -1);
+  let relative = resolveToAfterOrigin(prefix + joined);
+
+  const search = new RegExp(`^(${uniq})*`, 'g');
+  relative = relative.replace(search, (all: string) => {
+    const leftover = all.length / uniq.length;
+    return '../'.repeat(total - leftover);
+  });
+
+  return relative;
 }
 
 /**
@@ -44,38 +92,42 @@ export function resolve(input: string, base: string | undefined): string {
   // Absolute inputs are very easy to resolve right.
   if (isAbsoluteUrl(input)) return new Url(input).href;
 
-  // If it's protocol relative, we need to extract the protocol from the base,
-  // if it's absolute.
-  if (isProtocolRelativeUrl(input)) {
-    if (isAbsoluteUrl(base)) {
-      const { protocol } = new Url(base);
-      return new Url(protocol + input).href;
-    }
-    return resolveProtocolRelative(input, 'https://foo.com/');
-  }
-
   if (base) {
-    // The base should always end with a leading slash, to avoid errors with
-    // `https://foo.com` throwing.
-    base = withTrailingSlash(base);
+    // Bases are always implied to contain a directory path, Chrome and
+    // Mozilla's implementations.
+    if (!base.endsWith('/')) base += '/';
 
+    // Absolute paths are easy...
     if (isAbsoluteUrl(base)) return new Url(input, base).href;
+
+    // If it's protocl relative, we'll normalize it but keep it protocol
+    // relative.
     if (isProtocolRelativeUrl(base)) return resolveProtocolRelative(input, `https:${base}`);
   }
 
+  if (isProtocolRelativeUrl(input)) {
+    // If input is protocol relative, we just need to extract the protocol from
+    // the base if it's absolute.
+    if (isAbsoluteUrl(base)) return new Url(input, base).href;
+
+    // Else, we'll normalize it but keep it protocol relative.
+    return resolveProtocolRelative(input, 'https://foo.com/');
+  }
+
   // We now know both input and base are paths.
-  const resolved = pathResolve(base, input);
+  const resolved = resolvePath(input, base);
 
-  // If either were an absolute path, then the resolved path will already be an
-  // absolute path.
-  if (isPathAbsolute(input) || isPathAbsolute(base)) return resolved;
-
-  // Both were relative paths, and Node prepended the CWD, which we don't want.
-  const relative = pathRelative('', resolved);
+  // If either was an absolute path, our path resolver will have stripped the
+  // leading slash. Resolving it again is easy enough, plus it'll strip out any
+  // leading ".." that could have accumlated if input has more parent accessors
+  // than base had depth.
+  if (isPathAbsolute(input) || isPathAbsolute(base)) {
+    return '/' + resolveToAfterOrigin(resolved);
+  }
 
   // If either path started with a dot, let's keep the dot.
-  if (!relative.startsWith('.') && (base.startsWith('.') || input.startsWith('.'))) {
-    return `./${relative}`;
+  if (!resolved.startsWith('.') && (base.startsWith('.') || (!base && input.startsWith('.')))) {
+    return `./${resolved}`;
   }
-  return relative;
+  return resolved;
 }
--- test/unit/resolve.ts
+++ test/unit/resolve.ts
@@ -1,7 +1,29 @@
 import { URL } from 'url';
+import dirname from '../../src/dirname';
 import { resolve } from '../../src/resolve';
 
 describe('resolve', () => {
+  function expectEndsWith(actual: string, expected: string): void {
+    const sliced = actual.slice(-expected.length);
+    expect(sliced).toBe(expected);
+  }
+  function expectContains(actual: string, expected: string): void {
+    expect(actual.includes(expected)).toBe(true);
+  }
+
+  function stripOrigin(url: string): string {
+    let index = 0;
+    if (url.startsWith('https://')) {
+      index = url.indexOf('/', 'https://'.length);
+    } else if (url.startsWith('//')) {
+      index = url.indexOf('/', '//'.length);
+    } else {
+      return url;
+    }
+    expect(index).toBeGreaterThan(0);
+    return url.slice(index);
+  }
+
   function suite(base: string): void {
     describe(`base = "${base}"`, () => {
       describe('with absolute input', () => {
@@ -31,9 +53,7 @@ describe('resolve', () => {
 
         test('normalizes input', () => {
           const input = '//protocol-relative.com/foo/./bar/../main.js.map';
-          const expected = '//protocol-relative.com/foo/main.js.map';
-          const resolved = resolve(input, base);
-          expect(resolved.slice(-expected.length)).toBe('//protocol-relative.com/foo/main.js.map');
+          expectEndsWith(resolve(input, base), '//protocol-relative.com/foo/main.js.map');
         });
       });
 
@@ -41,17 +61,13 @@ describe('resolve', () => {
         test('remains absolute path', () => {
           const input = '/assets/main.js.map';
           const resolved = resolve(input, base);
-
-          const { pathname } = new URL(resolved, 'https://foo.com/');
-          expect(pathname).toBe(input);
+          expect(stripOrigin(resolved)).toBe(input);
         });
 
         test('normalizes input', () => {
           const input = '/foo/./bar/../main.js.map';
           const resolved = resolve(input, base);
-
-          const { pathname } = new URL(resolved, 'https://foo.com/');
-          expect(pathname).toBe('/foo/main.js.map');
+          expect(stripOrigin(resolved)).toBe('/foo/main.js.map');
         });
       });
 
@@ -60,29 +76,30 @@ describe('resolve', () => {
           test('resolves relative to current directory', () => {
             const input = './bar/main.js.map';
 
-            // TODO: we should assert that the base's path remains intact.
-            expect(resolve(input, base).endsWith('/bar/main.js.map')).toBe(true);
+            const resolved = resolve(input, base);
+            expectEndsWith(resolved, 'bar/main.js.map');
+            expectContains(resolved, base);
           });
 
           test('resolves relative to parent directory', () => {
             const input = '../bar/main.js.map';
 
             // TODO: we should assert that the base's path shortened by one dir.
-            expect(resolve(input, base).endsWith('bar/main.js.map')).toBe(true);
+            expectEndsWith(resolve(input, base), 'bar/main.js.map');
           });
 
           test('resolves relative to parent multiple directory', () => {
             const input = '../../../bar/main.js.map';
 
             // TODO: we should assert that the base's path shortened by three dirs.
-            expect(resolve(input, base).endsWith('bar/main.js.map')).toBe(true);
+            expectEndsWith(resolve(input, base), 'bar/main.js.map');
           });
 
           test('resolves relative to parent multiple directory, later', () => {
             const input = 'foo/../../../bar/main.js.map';
 
             // TODO: we should assert that the base's path shortened by two dirs.
-            expect(resolve(input, base).endsWith('bar/main.js.map')).toBe(true);
+            expectEndsWith(resolve(input, base), 'bar/main.js.map');
           });
         } else {
           test('remains relative to current directory', () => {
@@ -94,6 +111,12 @@ describe('resolve', () => {
           test('remains relative to parent directory', () => {
             const input = '../bar/main.js.map';
             expect(resolve(input, base)).toBe(input);
+          });
+
+          test('remains relative to parent directory, later', () => {
+            const input = 'foo/../../../bar/main.js.map';
+
+            expectEndsWith(resolve(input, base), '../../bar/main.js.map');
           });
         }
 
@@ -116,7 +139,6 @@ describe('resolve', () => {
     suite('https://foo.com/deep/file');
   });
 
-  // We need a better resolver to pass these tests
   describe('with protocol relative base', () => {
     suite('//foo.com');
     suite('//foo.com/file');
@@ -124,7 +146,12 @@ describe('resolve', () => {
     suite('//foo.com/deep/file');
   });
 
-  // We need a better resolver to pass these tests
+  describe('with path absolute base', () => {
+    suite('/');
+    suite('/root');
+    suite('/root/file');
+  });
+
   describe('with relative base', () => {
     suite('file');
     suite('deep/file');
@@ -132,5 +159,15 @@ describe('resolve', () => {
     suite('./deep/file');
     suite('../file');
     suite('../deep/file');
+  });
+
+  test('explicit base absolute path case', () => {
+    expect(resolve('../foo', '/')).toBe('/foo');
+  });
+
+  test('explicit base relative path case', () => {
+    expect(resolve('./foo', '')).toBe('./foo');
+    expect(resolve('foo', './a')).toBe('./a/foo');
+    expect(resolve('./foo', 'a')).toBe('a/foo');
   });
 });
