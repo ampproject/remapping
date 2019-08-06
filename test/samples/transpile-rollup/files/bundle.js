9f55c89b9974fb93983f8f4fefe58eb041c528d7
Justin Ridgewell <jridgewell@google.com>
2019-08-05 17:52:39 -0700

    Update license header

--- test/samples/transpile-rollup/files/bundle.js
+++ test/samples/transpile-rollup/files/bundle.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 /**
- * Copyright 2019 Google LLC
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ var a = (function () {
 });
 
 /**
- * Copyright 2019 Google LLC
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
