fa77c49cbe6e35a85d90c8197ace64409e6c2e65
Justin Ridgewell <justin@ridgewell.name>
2022-01-23 03:18:15 -0500

    Fix rollup config for umd binary

--- rollup.config.js
+++ rollup.config.js
@@ -16,16 +16,23 @@
 
 import typescript from '@rollup/plugin-typescript';
 
-
 function configure(esm) {
   return {
     input: 'src/remapping.ts',
     output: esm
       ? { format: 'es', dir: 'dist', entryFileNames: '[name].mjs', sourcemap: true }
-      : { format: 'umd', name: 'remapping', dir: 'dist', entryFileNames: '[name].umd.js', sourcemap: true },
-    plugins: [
-      typescript({ tsconfig: './tsconfig.build.json' }),
-    ],
+      : {
+          format: 'umd',
+          name: 'remapping',
+          dir: 'dist',
+          entryFileNames: '[name].umd.js',
+          sourcemap: true,
+          globals: {
+            '@jridgewell/resolve-uri': 'resolveURI',
+            'sourcemap-codec': 'sourcemapCodec',
+          },
+        },
+    plugins: [typescript({ tsconfig: './tsconfig.build.json' })],
     watch: {
       include: 'src/**',
     },
